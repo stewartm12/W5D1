e55d0f5bf20d112a41ca7c782c43bb56ecbf2414
John Cigale <jcigale98@gmail.com>
2021-01-25 13:54:12 -0500

    working on resizingintset

--- skeleton/lib/p01_int_set.rb
+++ skeleton/lib/p01_int_set.rb
@@ -30,23 +30,28 @@ end
 
 
 class IntSet
+  attr_reader :store
+  
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    self.store[num % num_buckets]
   end
 
   def num_buckets
@@ -55,7 +60,8 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_reader :store
+  attr_accessor :count
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
@@ -63,18 +69,22 @@ class ResizingIntSet
   end
 
   def insert(num)
+    return false if self.include?(num)
+    self[num] << num
+    self.count += 1
   end
 
   def remove(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    self.store[num % num_buckets]
   end
 
   def num_buckets
